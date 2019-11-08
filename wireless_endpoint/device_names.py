55a04389da5bce06bc86f8c26a78f3eff13bf450
Vincent De Maertelaere <vincent.demaertelaere@excentis.com>
2019-11-08 14:52:10 +0100

    Fixing py2/py3 compat

--- wireless_endpoint/device_names.py
+++ wireless_endpoint/device_names.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python
+from __future__ import print_function
 import byteblowerll.byteblower as byteblower
 import sys
 
-try:
-    instance = byteblower.ByteBlower.InstanceGet()
-    assert isinstance(instance, byteblower.ByteBlower)
 
-    # Connect to the meetingpoint
-    #meetingpoint = instance.MeetingPointAdd(sys.argv[2])
-    meetingpoint = instance.MeetingPointAdd(sys.argv[2])
+def print_device_info(server_address):
+    try:
+        instance = byteblower.ByteBlower.InstanceGet()
+        assert isinstance(instance, byteblower.ByteBlower)
 
-    weList = meetingpoint.DeviceListGet()
-    for we in weList:
-        print (we.DeviceInfoGet().GivenNameGet() + "  Battery Level: " + str(we.DeviceInfoGet().BatteryLevelGet()))
-except Exception as e:
-    print("Caught Exception: " + str(e))
+        # Connect to the meetingpoint
+        meetingpoint = instance.MeetingPointAdd(server_address)
+
+        wireless_endpoints = meetingpoint.DeviceListGet()
+        for we in wireless_endpoints:
+            print(we.DeviceInfoGet().GivenNameGet() + "  Battery Level: " + str(we.DeviceInfoGet().BatteryLevelGet()))
+
+    except Exception as e:
+        print("Caught Exception: " + str(e))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("ERROR: Expecting address of the server to connect to:")
+        print("  USAGE: {} <server_address>".format(sys.argv[0]))
+        sys.exit(1)
+
+    sys.exit(print_device_info(sys.argv[1]))
