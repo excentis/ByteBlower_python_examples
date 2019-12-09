89522f310446ca9f3a48027147acc104698b1616
Vincent De Maertelaere <vincent.demaertelaere@excentis.com>
2019-12-09 09:44:20 +0100

    Made the script a little more testable

--- demo_scripts/print_device_names.py
+++ demo_scripts/print_device_names.py
@@ -4,7 +4,8 @@ import byteblowerll.byteblower as byteblower
 import sys
 
 
-def print_device_info(server_address):
+def get_device_info(server_address):
+    output = ""
     try:
         instance = byteblower.ByteBlower.InstanceGet()
         assert isinstance(instance, byteblower.ByteBlower)
@@ -14,10 +15,14 @@ def print_device_info(server_address):
 
         wireless_endpoints = meetingpoint.DeviceListGet()
         for we in wireless_endpoints:
-            print(we.DeviceInfoGet().GivenNameGet() + "  Battery Level: " + str(we.DeviceInfoGet().BatteryLevelGet()))
+            fmt = "{NAME} Battery Level: {BATTERYLEVEL}"
+            line = fmt.format(NAME=we.DeviceInfoGet().GivenNameGet(), BATTERYLEVEL=we.DeviceInfoGet().BatteryLevelGet())
+            output += line + "\n"
 
     except Exception as e:
-        print("Caught Exception: " + str(e))
+        print("Caught Exception:", str(e))
+
+    return output
 
 
 if __name__ == '__main__':
@@ -26,4 +31,4 @@ if __name__ == '__main__':
         print("  USAGE: {} <server_address>".format(sys.argv[0]))
         sys.exit(1)
 
-    sys.exit(print_device_info(sys.argv[1]))
+    print(get_device_info(sys.argv[1]))
