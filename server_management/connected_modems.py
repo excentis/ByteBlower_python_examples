cba102935563118cada302c78aed2620723483b6
pieter.v <pieter vandercammen>
2020-01-24 18:24:16 +0100

    Add exception handling, do close the connection when finished

--- server_management/connected_modems.py
+++ server_management/connected_modems.py
@@ -30,16 +30,22 @@ def lookup_vendor_name(mac_address):
     """
         Translates the returned mac-address to a vendor
     """
-    # API base url,you can also use https if you need
+
     url = "http://macvendors.co/api/%s" % mac_address
-
     request = urllib2.Request(url, headers={'User-Agent': "API Browser"})
-    response = urllib2.urlopen(request)
+    try:
+        response = urllib2.urlopen(request)
 
-    # Fix: json object must be str, not 'bytes'
-    reader = codecs.getreader("utf-8")
-    obj = json.load(reader(response))
-    return obj['result']['company']
+        reader = codecs.getreader("utf-8")
+        obj = json.load(reader(response))
+        response.close()
+        return obj['result']['company']
+    except urllib2.URLError:
+        return "Unable to lookup MAC address"
+    except KeyError:
+        return "MAC lookup API changed"
+
+        
 
 
 def inspect_trunk(server, trunkbase):
