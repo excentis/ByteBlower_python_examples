f62a4c680861bcfd72891bb569ebb697fe68311b
Vincent De Maertelaere <vincent.demaertelaere@excentis.com>
2020-10-02 13:19:50 +0200

    Fixing some typos, copy-paste errors and made it fit into our CI platform

--- back2back/multiple_clients.py
+++ back2back/multiple_clients.py
@@ -7,15 +7,17 @@ This example assumes that you already familiar with
 the basic ByteBlower API for HTTP/TCP traffic. 
 As you will notice, adding more TCP clients is easy!
 """
+
 # Needed for python2 / python3 print function compatibility
 from __future__ import print_function
 
-# import the ByteBlower module
-import byteblowerll.byteblower as byteblower
-from byteblowerll.byteblower import HTTPRequestStatus
 import time
 
-
+# import the ByteBlower module
+from byteblowerll.byteblower import ByteBlower, RequestStartType
+from byteblowerll.byteblower import ConvertHTTPRequestStatusToString
+from byteblowerll.byteblower import HTTPRequestMethod, HTTPRequestStatus
+from byteblowerll.byteblower import ParseHTTPRequestMethodFromString
 
 configuration = {
     # Address (IP or FQDN) of the ByteBlower server to use
@@ -25,7 +27,7 @@ configuration = {
     'server_bb_port': {
         'interface': 'trunk-1-20',
         'mac': '00:bb:01:00:00:01',
-        'ip' : 'dhcpv4',
+        'ip': 'dhcpv4',
 
         # The TCP Server listens new connections on this Port number.
         'tcp_listen_port': 4096
@@ -37,21 +39,19 @@ configuration = {
         'interface': 'trunk-1-14',
         'mac': '00:bb:01:00:00:02',
 
-        'ip' : 'dhcpv4',
+        'ip': 'dhcpv4',
     },
-    
-    # A list of HTTP clients. These will all start on 
-    # on the client ByteBlower Port.
+
+    # A list of HTTP clients. These will all start on on the client
+    # ByteBlower Port.
     # Each HTTP Client has a:
-    #     - http_method, this controls the direciton of
-    #           the traffic.
-    #     - the duration, this is one of available stond
-    #           conditions of a HTTP Server.
+    #     - http_method, this controls the direction of the traffic.
+    #     - the duration, this is one of available stop conditions of a
+    #       HTTP Server.
     #
-    # Each client is timebased, hence the duration parameter. Just
-    # as for the basic TCP example, this parameter uses nano-seconds
-    # as unit.
-    'http_clients' :[
+    # Each client is time based, hence the duration parameter. Just as for the
+    # basic TCP example, this parameter uses nano-seconds as unit.
+    'http_clients': [
         {'http_method': 'GET',
          'duration': 5000000000},
         {'http_method': 'PUT',
@@ -68,34 +68,38 @@ class Example:
         self.server_bb_port_config = kwargs['server_bb_port']
         self.client_bb_port_config = kwargs['client_bb_port']
 
-        # Helper function, we can use this to parse the HTTP Method to the
-        # enumeration used by the API
-        from byteblowerll.byteblower import ParseHTTPRequestMethodFromString
-
         self.http_client_configs = []
         for a_client in kwargs['http_clients']:
-            http_method = ParseHTTPRequestMethodFromString(a_client['http_method'])
-            duration = a_client['duration']
-            self.http_client_configs.append({'http_method': http_method, 'duration': duration})
+            config_http_method = a_client['http_method']
+            config_duration = a_client['duration']
+            http_method = ParseHTTPRequestMethodFromString(config_http_method)
+            self.http_client_configs.append({
+                'http_method': http_method,
+                'duration': config_duration
+            })
+
+        self.server = None
+        self.server_bb_port = None
+        self.client_bb_port = None
 
     def run(self):
-        byteblower_instance = byteblower.ByteBlower.InstanceGet()
+        byteblower_instance = ByteBlower.InstanceGet()
 
-        print("Connecting to ByteBlower server {}...".format(self.server_address))
+        print("Connecting to ByteBlower server %s..." % self.server_address)
         self.server = byteblower_instance.ServerAdd(self.server_address)
 
         # Create the port which will be the HTTP server (port_1)
         print("Creating HTTP Server port")
-        server_bb_port = self.provision_port(self.server_bb_port_config)
+        self.server_bb_port = self.provision_port(self.server_bb_port_config)
 
         print("Creating HTTP Client port")
         # Create the port which will be the HTTP client (port_2)
-        client_bb_port = self.provision_port(self.client_bb_port_config)
+        self.client_bb_port = self.provision_port(self.client_bb_port_config)
 
         http_server_ip_address = self.server_bb_port_config['ip_address']
 
         # create a HTTP server
-        http_server = server_bb_port.ProtocolHttpServerAdd()
+        http_server = self.server_bb_port.ProtocolHttpServerAdd()
         server_tcp_port = self.server_bb_port_config['tcp_listen_port']
         http_server.PortSet(server_tcp_port)
 
@@ -107,7 +111,8 @@ class Example:
         # We will create multiple clients onto the same the ByteBlowerPort.
         http_clients = []
 
-        # We'll use the max_duration for the stopcondtion furhter in the script. 
+        # We'll use the max_duration for the stop condition further in
+        # the script.
         max_duration = 0
 
         # Configure each client one-by-one.
@@ -116,9 +121,9 @@ class Example:
             # create a new HTTP Client and configure it.
             # This part is the same for 1 or multiple ones.
             #
-            # You can configure multiple clients to connecto to the 
+            # You can configure multiple clients to connect to the
             # the same server. You can even mix different HTTP Methods.
-            http_client = client_bb_port.ProtocolHttpClientAdd()
+            http_client = self.client_bb_port.ProtocolHttpClientAdd()
 
             http_client.RemoteAddressSet(http_server_ip_address)
             http_client.RemotePortSet(server_tcp_port)
@@ -126,27 +131,30 @@ class Example:
             http_client.HttpMethodSet(client_config['http_method'])
             http_client.RequestDurationSet(client_config['duration'])
 
-            max_duration = max(max_duration, client_config['duration'] / 1e9)
+            # The client configured duration is in nanoseconds, we want to keep
+            # the max_duration as simple as 'seconds', so convert the value.
+            client_duration_s = client_config['duration'] / 1e9
+            max_duration = max(max_duration, client_duration_s)
 
             # The RequestStartType is the main configuration difference 
             # Between 2 TCP clients and multiple ones. Rather than starting
             # the client directly, we'll schedule when to start.
-            http_client.RequestStartTypeSet(byteblower.RequestStartType.Scheduled)
+            http_client.RequestStartTypeSet(RequestStartType.Scheduled)
             http_clients.append(http_client)
 
-        print("Server port:", server_bb_port.DescriptionGet()) 
-        print("Client port:", client_bb_port.DescriptionGet()) 
-       
+        print("Server port:", self.server_bb_port.DescriptionGet())
+        print("Client port:", self.client_bb_port.DescriptionGet())
+
         # This is another difference with the basic TCP example.
-        # The Start method on a ByteBlower Port starts all scheduled traffic types.
-        # For this example this means all configured HTTPClients.
-        client_bb_port.Start()
+        # The Start method on a ByteBlower Port starts all scheduled traffic
+        # types.  This example this means all configured HTTPClients.
+        self.client_bb_port.Start()
 
         # Unlike before we now have several HTTPClients running together.
         # This makes determining when the scenario is finished more 
         # complex.
         # Below we show two different stop conditions:
-        #    1. a timebased one. (required)
+        #    1. a time based one. (required)
         #    2. a client based one. (optional)
         #
         # Always use the time stop-condition. This provides a guaranteed
@@ -156,73 +164,102 @@ class Example:
         # to make the actual connection.
         extra_time = 2
         start_moment = time.time()
-        while (time.time() - start_moment) > (extra_time + duration):
+        time_elapsed = 0
+        while time_elapsed <= (extra_time + max_duration):
             time.sleep(1)
 
-            duration = (time.time() - start_moment) 
-            print('%.2fs :: Waiting for clients to finish.' % (duration))
+            time_elapsed = time.time() - start_moment
+            print('%.2fs :: Waiting for clients to finish.' % time_elapsed)
 
-            # Below is the second type of stopcondition, a client based
-            # one.
-            any_client_still_running = False 
+            # Below is the second type of stop condition, a client based one.
+            any_client_running = False
+
+            # Consider a client finished when the state is either:
+            # - Finished
+            # - Error
+            finished_states = [
+                HTTPRequestStatus.Error,
+                HTTPRequestStatus.Finished
+            ]
+
             for http_client in http_clients:
-                # Uptdate the local API object with the 
-                # with the info on the ByteBlowerServer.
+                # Update the local API object with the  with the info on the
+                # ByteBlowerServer.
                 http_client.Refresh()
 
                 # Check the status.
                 status = http_client.RequestStatusGet()
-                any_client_still_running = (any_client_still_running or 
-                                              not (HTTPRequestStatus.Finished == status or 
-                                                   HTTPRequestStatus.Error == status))
-            if not any_client_still_running:
+                this_client_running = status not in finished_states
+                any_client_running = any_client_running or this_client_running
+
+            # all clients finished?  No need to wait any longer.
+            if not any_client_running:
                 break
 
         # Stop the HTTP Server. 
-        # This step is optional but has the advantage of 
-        #  stopping traffic to/from the HTTP Clients
+        # This step is optional but has the advantage of stopping traffic
+        # to/from the HTTP Clients
         http_server.Stop()
+
+        server_number_of_clients = len(http_server.ClientIdentifiersGet())
 
         print("")
         print("HTTP Server info     ")
         print("-" * 10)
 
-        print("Connected clients     : {} ".format(len(http_server.ClientIdentifiersGet())))
+        print("Connected clients     : {} ".format(server_number_of_clients))
         print("")
 
         print("HTTP Client info     ")
         print("-" * 10)
 
         # Process each of the clients.
+        results = []
         for client in http_clients:
-            self.process_http_client(client)
+            results.append(self.process_http_client(client))
 
-        # Removing the server will also cleanup all of the 
-        # ByteBlower API objects.
-        byteblower_instance.ServerRemove(self.server)            
+        return results
 
+    def cleanup(self):
+        if self.client_bb_port:
+            self.server.PortDestroy(self.client_bb_port)
+            self.client_bb_port = None
+        if self.server_bb_port:
+            self.server.PortDestroy(self.server_bb_port)
+            self.server_bb_port = None
 
-    def process_http_client(self, http_client):        
+        # Disconnect from the ByteBlower server
+        if self.server:
+            ByteBlower.InstanceGet().ServerRemove(self.server)
+            self.server = None
+
+    @classmethod
+    def process_http_client(cls, http_client):
         """
             This method processes all results from an HTTPClient.
             The method is very similar to the one in the TCP example.
         """
-        named_http_methods = {byteblower.HTTPRequestMethod.Put: 'PUT',
-                        byteblower.HTTPRequestMethod.Get: 'GET'}
+        named_http_methods = {HTTPRequestMethod.Put: 'PUT',
+                              HTTPRequestMethod.Get: 'GET'}
+        local_tcp_port = http_client.LocalPortGet()
+        http_method = http_client.HttpMethodGet()
+        http_method_string = named_http_methods[http_method]
+        requested_duration = http_client.RequestDurationGet()
 
-        print("Local TCP Port        : {} ".format(http_client.LocalPortGet()))
-        print("Direction             : {} ".format(named_http_methods[http_client.HttpMethodGet()]))
-        print("Requested Duration    : {} nanoseconds".format(http_client.RequestDurationGet()))
+        print("Local TCP Port     : {} ".format(local_tcp_port))
+        print("Direction          : {} ".format(http_method_string))
+        print("Requested Duration : {} nanoseconds".format(requested_duration))
 
         request_status_value = http_client.RequestStatusGet()
 
+        status = ConvertHTTPRequestStatusToString(request_status_value)
         if not http_client.HasSession():
-            print("Status                : {}".format(byteblower.ConvertHTTPRequestStatusToString(request_status_value)))
+            print("Status                : {}".format(status))
             print("")
 
         # The HTTPSession has all the OSI Layer 5 info.
         http_client_session_info = http_client.HttpSessionInfoGet()
-        # Retrive the latest info from the Server.
+        # Retrieve the latest info from the Server.
         http_client_session_info.Refresh()
 
         http_session_info = http_client.HttpSessionInfoGet()
@@ -233,23 +270,27 @@ class Example:
         rx_bytes = http_result.RxByteCountTotalGet()
         avg_throughput = http_result.AverageDataSpeedGet()
 
-
-        # The TCP Session has all the OSI Layer 4 information. 
+        # The TCP Session has all the OSI Layer 4 information.
         tcp_result = http_session_info.TcpSessionInfoGet().ResultGet()
         tcp_result.Refresh()
 
         min_congestion = tcp_result.CongestionWindowMinimumGet()
         max_congestion = tcp_result.CongestionWindowMaximumGet()
-        
+        from byteblowerll.byteblower import DataRate
+        assert isinstance(avg_throughput, DataRate)
+
         print("TX Payload            : {} bytes".format(tx_bytes))
         print("RX Payload            : {} bytes".format(rx_bytes))
         print("Average Throughput    : {}".format(avg_throughput.toString()))
         print("Min Congestion Window : {} bytes".format(min_congestion))
         print("Max Congestion Window : {} bytes".format(max_congestion))
-        print("Status                : {}".format(byteblower.ConvertHTTPRequestStatusToString(request_status_value)))
+        print("Status                : {}".format(status))
         print("")
 
-        return [tx_bytes, rx_bytes, avg_throughput.toString(), min_congestion, max_congestion, request_status_value]
+        return [
+            tx_bytes, rx_bytes, avg_throughput.bitrate(),
+            min_congestion, max_congestion, request_status_value
+        ]
 
     def provision_port(self, config):
         port = self.server.PortCreate(config['interface'])
@@ -307,4 +348,8 @@ class Example:
 # called.  This approach makes it possible to include it in a series of
 # examples.
 if __name__ == "__main__":
-    Example(**configuration).run()
+    example = Example(**configuration)
+    try:
+        example.run()
+    finally:
+        example.cleanup()
