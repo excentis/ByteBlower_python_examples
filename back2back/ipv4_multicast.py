3bc4007906b450313b81a2ae6ad9d5cd8368afb1
francis <francis.rammeloo@excentis.com>
2022-10-05 16:20:11 +0200

    Add ipv4_multicast.

---
+++ back2back/ipv4_multicast.py
@@ -0,0 +1,279 @@
+"""
+Basic IPv4 multicast example using the ByteBlower Python API.
+
+Copyright 2022, Excentis N.V.
+"""
+
+from __future__ import print_function
+
+import math
+from time import sleep
+
+from byteblowerll.byteblower import ByteBlower
+
+configuration = {
+    # Address (IP or FQDN) of the ByteBlower server to use
+    'server_address': 'byteblower-dev-4100-3.lab.byteblower.excentis.com',
+
+    # Configuration for the first ByteBlower port.
+    # Will be used as the TX port.
+    'port_1_config': {
+        'interface': 'nontrunk-1',
+        'mac': '00:bb:01:00:00:01',
+        # IP configuration for the ByteBlower Port.  Only IPv4 is supported
+        # Options are 'DHCPv4', 'static'
+        # if DHCPv4, use "dhcpv4"
+        # 'ip': 'dhcpv4',
+        # if staticv4, use ["ipaddress", netmask, gateway]
+        'ip': ['192.168.0.2', "255.255.255.0", "192.168.0.1"],
+    },
+
+    # Configuration for the second ByteBlower port.
+    # Will be used as RX port.
+    'port_2_config': {
+        'interface': 'nontrunk-2',
+        'mac': '00:bb:01:00:00:02',
+        # IP configuration for the ByteBlower Port.  Only IPv4 is supported
+        # Options are 'DHCPv4', 'static'
+        # if DHCPv4, use "dhcpv4"
+        # 'ip': 'dhcpv4',
+        # if staticv4, use ["ipaddress", netmask, gateway]
+        'ip': ['192.168.0.2', "255.255.255.0", "192.168.0.1"],
+    },
+
+
+    # Size of the frames to be sent (without CRC).
+    # Unit: Bytes
+    'frame_size': 512,
+
+    # Sending 100 frames per second
+    'interframegap_nanoseconds': 10000000,  # 10ms
+
+    # Send traffic for 5 minutes
+    'number_of_frames': 30000,
+
+    # Instead of configuring the inter-frame gap above, one can specify a
+    # desired throughput too.  This conversion will be done in the __init__
+    # function of the example.
+    # When set, the throughput is used, otherwise interframegap above will
+    # be used.
+    # Units: Mbit/s
+    # 'throughput': 400
+}
+
+
+class Example:
+    def __init__(self, **kwargs):
+        self.server_address = kwargs['server_address']
+        self.port_1_config = kwargs['port_1_config']
+        self.port_2_config = kwargs['port_2_config']
+
+        self.number_of_frames = kwargs['number_of_frames']
+        self.frame_size = kwargs['frame_size']
+
+        self.interframegap_ns = kwargs['interframegap_nanoseconds']
+
+        self.server = None
+        self.bbport_tx = None
+        self.bbport_rx = None
+
+    def cleanup(self):
+        """Clean up the created objects"""
+        byteblower_instance = ByteBlower.InstanceGet()
+        if self.bbport_tx:
+            self.server.PortDestroy(self.bbport_tx)
+            self.bbport_tx = None
+
+        if self.bbport_rx:
+            self.server.PortDestroy(self.bbport_rx)
+            self.bbport_rx = None
+
+        if self.server is not None:
+            byteblower_instance.ServerRemove(self.server)
+            self.server = None
+
+    def generate_frame_string(self, src_mac, src_ip, udp_src_port, dst_mac, dst_ip, udp_dst_port):
+        ethernet_header_len = 14
+        ip_header_len = 20
+        udp_header_len = 8
+        total_header_len = ethernet_header_len + ip_header_len + udp_header_len
+        payload = 'a' * (self.frame_size - total_header_len)
+
+        from scapy.layers.inet import UDP, IP, Ether
+        from scapy.all import Raw
+        udp_payload = Raw(payload.encode('ascii', 'strict'))
+        udp_header = UDP(dport=udp_dst_port, sport=udp_src_port)
+        ip_header = IP(src=src_ip, dst=dst_ip)
+        eth_header = Ether(src=src_mac, dst=dst_mac)
+        scapy_frame = eth_header / ip_header / udp_header / udp_payload
+
+        frame_content = bytearray(bytes(scapy_frame))
+
+        # The ByteBlower API expects an 'str' as input for the
+        # frame::BytesSet() method, we need to convert the bytearray
+        return ''.join((format(b, "02x") for b in frame_content))
+
+    def run(self):
+        udp_src_port = 5001
+        udp_dst_port = 5002
+
+        byteblower_instance = ByteBlower.InstanceGet()
+
+        print("Connecting to ByteBlower server %s..." % self.server_address)
+        self.server = byteblower_instance.ServerAdd(self.server_address)
+
+        # Create the sending port
+        print("Creating TX port")
+        self.bbport_tx = self.provision_port(self.port_1_config)
+
+        print("Creating RX port")
+        # Create the port which will be the HTTP client (port_2)
+        self.bbport_rx = self.provision_port(self.port_2_config)
+
+        # now create the stream.
+        # A stream transmits frames on the port on which it is created.
+        stream = self.bbport_tx.TxStreamAdd()
+        stream.NumberOfFramesSet(self.number_of_frames)
+        stream.InterFrameGapSet(self.interframegap_ns)
+        frame = stream.FrameAdd()
+
+        # collect the frame header info.  We need to provide the Layer2
+        # (ethernet) and Layer3 (IPv4) addresses.
+        src_ip = self.port_1_config['ip_address']
+        src_mac = self.bbport_tx.Layer2EthIIGet().MacGet()
+        dst_ip = self.port_2_config['ip_address']
+        dst_mac = self.bbport_tx.Layer3IPv4Get().Resolve(dst_ip)
+
+        frame.BytesSet(self.generate_frame_string(src_mac, src_ip, udp_src_port, dst_mac, dst_ip, udp_dst_port))
+
+        # create a trigger.  A trigger is an object which receives data.
+        # The Basic trigger just count packets
+        trigger = self.bbport_rx.RxTriggerBasicAdd()
+
+        # every trigger needs to know on which frames it will work.
+        # The default filter is no filter, so it will analyze every frame,
+        # which is not what we want here.
+        # We will filter on the destination IP and the destination UDP port
+        bpf_filter = "ip dst {} and udp dst port {}".format(dst_ip, udp_dst_port)
+        trigger.FilterSet(bpf_filter)
+
+        # print the configuration.
+        # his makes it easy to review what we have done until now
+        print("Current ByteBlower configuration:")
+        print("port1:", self.bbport_tx.DescriptionGet())
+        print("port2:", self.bbport_rx.DescriptionGet())
+
+        # start the traffic and clear the trigger.
+        # Triggers are active as soon they are created, so we may want to clear
+        # the data it already has collected.
+        print("Starting traffic")
+        trigger.ResultClear()
+        stream_history = stream.ResultHistoryGet()
+        trigger_history = trigger.ResultHistoryGet()
+
+        duration_ns = self.interframegap_ns * self.number_of_frames
+        duration_s = duration_ns / 1000000000 + 1
+
+        stream.Start()
+
+        # duration_s is a float, so we need to cast it to an integer first
+        for iteration in range(1, int(duration_s)):
+            # sleep one second
+            sleep(1)
+
+            # Refresh the history, the ByteBlower server will create interval
+            # and cumulative results every second (by default).  The Refresh()
+            # method will synchronize the server data with the client.
+            stream_history.Refresh()
+            trigger_history.Refresh()
+
+            last_interval_tx = stream_history.IntervalLatestGet()
+            last_interval_rx = trigger_history.IntervalLatestGet()
+
+            print("Sent {TX} frames, received {RX} frames".format(
+                TX=last_interval_tx.PacketCountGet(),
+                RX=last_interval_rx.PacketCountGet()
+            ))
+
+        print("Done sending traffic (time elapsed)")
+
+        # Waiting for a second after the stream is finished.
+        # This has the advantage that frames that were transmitted
+        # but not received yet, can be processed by the server
+        print("Waiting for a second")
+        sleep(1)
+
+        # During the test itself we queried the interval counters,
+        # there are also cumulative counters.  The last cumulative counter
+        # available in the history is also available as the Result
+        stream_result = stream.ResultGet()
+        stream_result.Refresh()
+        print("Stream result:", stream_result.DescriptionGet())
+
+        trigger_result = trigger.ResultGet()
+        trigger_result.Refresh()
+        print("Trigger result:", trigger_result.DescriptionGet())
+
+        tx_frames = stream_result.PacketCountGet()
+        rx_frames = trigger_result.PacketCountGet()
+
+        print("Sent {TX} frames, received {RX} frames".format(TX=tx_frames, RX=rx_frames))
+
+        return [tx_frames, rx_frames]
+
+    def provision_port(self, config):
+        port = self.server.PortCreate(config['interface'])
+        port_l2 = port.Layer2EthIISet()
+        port_l2.MacSet(config['mac'])
+
+        ip_config = config['ip']
+        if not isinstance(ip_config, list):
+            # Config is not static, DHCP or slaac
+            if ip_config.lower() == "dhcpv4":
+                port_l3 = port.Layer3IPv4Set()
+                port_l3.ProtocolDhcpGet().Perform()
+                config['ip_address'] = port_l3.IpGet()
+            elif ip_config.lower() == "dhcpv6":
+                port_l3 = port.Layer3IPv6Set()
+                port_l3.ProtocolDhcpGet().Perform()
+                config['ip_address'] = port_l3.IpDhcpGet()
+            elif ip_config.lower() == "slaac":
+                port_l3 = port.Layer3IPv6Set()
+                port_l3.StatelessAutoconfiguration()
+                config['ip_address'] = port_l3.IpStatelessGet()
+        else:
+            # Static configuration
+            if len(ip_config) == 3:
+                # IPv4
+                port_l3 = port.Layer3IPv4Set()
+                port_l3.IpSet(ip_config[0])
+                port_l3.NetmaskSet(ip_config[1])
+                port_l3.GatewaySet(ip_config[2])
+                config['ip_address'] = port_l3.IpGet()
+            elif len(ip_config) == 2:
+                port_l3 = port.Layer3IPv6Set()
+                # IPv6
+                address = ip_config[0]
+                prefix_length = ip_config[1]
+                ip = "{}/{}".format(address, prefix_length)
+                port_l3.IpManualAdd(ip)
+                config['ip_address'] = ip_config[0]
+
+        if not isinstance(config['ip_address'], str):
+            ip = config['ip_address'][0]
+            if '/' in ip:
+                config['ip_address'] = ip.split('/')[0]
+
+        print("Created port", port.DescriptionGet())
+        return port
+
+
+# When this python module is called stand-alone, the run-function must be
+# called.  This approach makes it possible to include it in a series of
+# examples.
+if __name__ == "__main__":
+    example = Example(**configuration)
+    try:
+        example.run()
+    finally:
+        example.cleanup()
